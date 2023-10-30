db7fdafec3f942a3dfb0c7b4b6c280c47ee5ee3f
Sagarshah68 <130995184+Sagarshah68@users.noreply.github.com>
2023-10-30 20:23:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showPopup() {
+    alert("Congratulations! You've taken the first step to a better life with our awesome product!");
+}
+
+// You can add more JavaScript functionalities as needed.
